b485977a5f9f7dc6ea9b69846f3c7a22bbec5193
Aika-Kawase <ee223067@meiji.ac.jp>
2026-01-03 06:01:57 +0000

    add suitable topic for sim

--- wild_visual_navigation_ros/scripts/camera_info_publisher.py
+++ wild_visual_navigation_ros/scripts/camera_info_publisher.py
@@ -8,70 +8,71 @@ def publish_camera_info():
     rospy.init_node('camera_info_publisher', anonymous=True)
 
     # whill
-    info_topic_left = "/usb_cam/camera_info_selfpublished"
-
-    camera_info_pub_left = rospy.Publisher(info_topic_left, CameraInfo, queue_size=10)
-
-    rate = rospy.Rate(10) # 10Hzでパブリッシュ
-
-    camera_info_left_msg = CameraInfo()
-    camera_info_left_msg.header = Header()
-    camera_info_left_msg.header.frame_id = "use_cam"
-    camera_info_left_msg.width = 1280
-    camera_info_left_msg.height = 720
-    
-    # カメラの内部パラメータ行列 (K)
-    # [fx 0 cx]
-    # [0 fy cy]
-    # [0 0 1 ]
-    camera_info_left_msg.K = [1007.544100, 0.0, 687.923065,
-                         0.0, 1006.521613, 376.590237,
-                         0.0, 0.0, 1.0]
-
-    # カメラの歪み係数 (D)
-    # TartanDriveは歪みがないため、すべてゼロ
-    camera_info_left_msg.D = [0.046912, -0.088308, 0.001497, 0.011384, 0.000000]
-
-    # 歪みの種類
-    camera_info_left_msg.distortion_model = "plumb_bob"
-
-
-    # ここにCameraInfoをパブリッシュするトピック名を設定
-    # info_topic_left = "/multisense/left/camera_info"
-    # info_topic_right = "/multisense/right/camera_info"
+    # info_topic_left = "/usb_cam/camera_info_selfpublished"
 
     # camera_info_pub_left = rospy.Publisher(info_topic_left, CameraInfo, queue_size=10)
-    # camera_info_pub_right = rospy.Publisher(info_topic_right, CameraInfo, queue_size=10)
 
     # rate = rospy.Rate(10) # 10Hzでパブリッシュ
 
-    info_topic_right = "/multisense/right/camera_info"
-    camera_info_pub_right = rospy.Publisher(info_topic_right, CameraInfo, queue_size=10)
-
-
-    # tartan
-    # # Tartan Driveデータセットのカメラパラメータ
-    # # left
     # camera_info_left_msg = CameraInfo()
     # camera_info_left_msg.header = Header()
-    # camera_info_left_msg.header.frame_id = "multisense/left_camera_optical_frame"
-    # camera_info_left_msg.width = 1024
-    # camera_info_left_msg.height = 544
+    # camera_info_left_msg.header.frame_id = "use_cam"
+    # camera_info_left_msg.width = 1280
+    # camera_info_left_msg.height = 720
     
     # # カメラの内部パラメータ行列 (K)
     # # [fx 0 cx]
     # # [0 fy cy]
     # # [0 0 1 ]
-    # camera_info_left_msg.K = [477.605, 0.0, 499.5,
-    #                      0.0, 477.605, 252.0,
+    # camera_info_left_msg.K = [1007.544100, 0.0, 687.923065,
+    #                      0.0, 1006.521613, 376.590237,
     #                      0.0, 0.0, 1.0]
 
     # # カメラの歪み係数 (D)
     # # TartanDriveは歪みがないため、すべてゼロ
-    # camera_info_left_msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
+    # camera_info_left_msg.D = [0.046912, -0.088308, 0.001497, 0.011384, 0.000000]
 
     # # 歪みの種類
     # camera_info_left_msg.distortion_model = "plumb_bob"
+
+
+    # tartan
+    # ここにCameraInfoをパブリッシュするトピック名を設定
+    info_topic_left = "/multisense/left/camera_info"
+    info_topic_right = "/multisense/right/camera_info"
+
+    camera_info_pub_left = rospy.Publisher(info_topic_left, CameraInfo, queue_size=10)
+    camera_info_pub_right = rospy.Publisher(info_topic_right, CameraInfo, queue_size=10)
+
+    rate = rospy.Rate(10) # 10Hzでパブリッシュ
+
+    # whill & tartan
+    info_topic_right = "/multisense/right/camera_info"
+    camera_info_pub_right = rospy.Publisher(info_topic_right, CameraInfo, queue_size=10)
+
+
+    # tartan
+    # left
+    camera_info_left_msg = CameraInfo()
+    camera_info_left_msg.header = Header()
+    camera_info_left_msg.header.frame_id = "multisense/left_camera_optical_frame"
+    camera_info_left_msg.width = 1024
+    camera_info_left_msg.height = 544
+    
+    # カメラの内部パラメータ行列 (K)
+    # [fx 0 cx]
+    # [0 fy cy]
+    # [0 0 1 ]
+    camera_info_left_msg.K = [477.605, 0.0, 499.5,
+                         0.0, 477.605, 252.0,
+                         0.0, 0.0, 1.0]
+
+    # カメラの歪み係数 (D)
+    # TartanDriveは歪みがないため、すべてゼロ
+    camera_info_left_msg.D = [0.0, 0.0, 0.0, 0.0, 0.0]
+
+    # 歪みの種類
+    camera_info_left_msg.distortion_model = "plumb_bob"
 
 
     # right
